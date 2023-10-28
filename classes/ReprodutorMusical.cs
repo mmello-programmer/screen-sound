b0e86bae7a356b778419acf268644200fb891af0
Marco Mello <marcomello140365@gmail.com>
2023-10-28 01:49:52 -0300

    Primeiro commit, projeto SS (Alura)

---
+++ classes/ReprodutorMusical.cs
@@ -0,0 +1,33 @@
+public class ReprodutorMusical {
+
+public void ReproducaoDeMusicas()
+{
+
+}
+
+public void IncluindoReproducao()
+{
+
+}
+
+public void Pausa()
+{
+
+}
+
+public void Avanco()
+{
+
+}
+
+public void Retrocesso()
+{
+
+}
+
+public void ControleDeVolume()
+{
+
+}
+
+}
